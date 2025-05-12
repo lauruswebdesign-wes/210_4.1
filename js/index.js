0beff223737d30bb58e8bda8b516d007db66e25d
Wes <wesley.nimmo@lauruscollege.edu>
2025-05-12 16:00:34 -0700

    Finalized project for github

---
+++ js/index.js
@@ -0,0 +1,8 @@
+let heroBTN = document.querySelector('.hero-btn')
+let modal = document.querySelector('.modal')
+
+heroBTN.addEventListener('click', () => {
+
+  modal.classList.add('show-modal')
+
+})
