4db99fa500e1530d2a3851487a2644d7e5384b1b
Wes <wesley.nimmo@lauruscollege.edu>
2025-05-12 16:31:36 -0700

    Finishing touches

--- js/index.js
+++ js/index.js
@@ -1,8 +1,30 @@
 let heroBTN = document.querySelector('.hero-btn')
 let modal = document.querySelector('.modal')
+let modalBTN = document.querySelector('.modal-btn')
+let closeModal = document.querySelector('.close-modal')
+
 
 heroBTN.addEventListener('click', () => {
 
   modal.classList.add('show-modal')
+ 
 
 })
+
+modalBTN.addEventListener('click', () => {
+
+  modal.classList.remove('show-modal')
+ 
+
+})
+
+closeModal.addEventListener('click', () => {
+
+  modal.classList.remove('show-modal')
+ 
+
+})
+
+
+
+
